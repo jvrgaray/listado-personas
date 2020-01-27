2672befdf170d73234a15a6a4db379cbaf08d1ca
Javier Garay <javier.garay@2brains.cl>
2020-01-27 16:58:25 -0300

    47. Routing en Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { AppRoutingModule } from './app-routing/app-routing.module';
 import { PersonasServices } from './personas.service';
 import { LoggingService } from './LoggingService.service';
 import { BrowserModule } from '@angular/platform-browser';
@@ -7,16 +8,19 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { PersonaComponent } from './persona/persona.component';
 import { FormularioComponent } from './formulario/formulario.component';
+import { PersonasComponent } from './personas/personas.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PersonaComponent,
-    FormularioComponent
+    FormularioComponent,
+    PersonasComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [LoggingService, PersonasServices],
   bootstrap: [AppComponent]

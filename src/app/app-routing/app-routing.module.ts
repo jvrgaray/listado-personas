d5b0bf5ede9aabed9a16baa148495c88119d2d08
Javier Garay <javier.garay@2brains.cl>
2020-01-28 17:06:12 -0300

    54. Child route en angular

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -5,9 +5,13 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   { path: '', component: PersonasComponent },
-  { path: 'personas', component: PersonasComponent },
-  { path: 'personas/agregar', component: FormularioComponent },
-  { path: 'personas/:id', component: FormularioComponent } // editar
+  {
+    path: 'personas', component: PersonasComponent,
+    children: [
+      { path: 'agregar', component: FormularioComponent },
+      { path: ':id', component: FormularioComponent } // editar
+    ]
+  }
 
 ];
 
